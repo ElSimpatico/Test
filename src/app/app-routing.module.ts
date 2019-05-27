2639a6c0f377e4d310ea39dff89c8d67d96b41ff
Aaron Velasco <tiquismiquisss@gmail.com>
2019-05-28 00:22:52 +0200

    lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: './modules/home/home.module#HomeModule'
+  },
+  {
+    path: 'contact',
+    loadChildren: './modules/contact/contact.module#ContactModule'
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
